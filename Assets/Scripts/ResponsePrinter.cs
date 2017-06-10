614a7748336df21659fb975554c2dc1d060b26ed
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-06-10 07:04:39 -0400

    Builds UI elements from Silk code

---
+++ Assets/Scripts/ResponsePrinter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Silk;
+public class ResponsePrinter : MonoBehaviour {
+    //attach to container of responses
+    
+    public GameObject responsePrefab;
+    private void Start() {
+         foreach(KeyValuePair<string, SilkGraph> story in Parser.Instance.mother.MotherGraph) {
+            foreach(KeyValuePair<string, SilkNode> node in story.Value.Story) {
+                if(node.Value.GetNodeName() == "Start") {
+                    for(int i = 0; i < node.Value.silkLinks.Count; i++) {
+                        GameObject newResponse = Instantiate(responsePrefab);
+                        //means this script must be attached to the parent object
+                        newResponse.transform.SetParent(this.gameObject.transform);
+                        newResponse.transform.localScale = new Vector3(1, 1, 1);
+                        newResponse.GetComponent<Response>().responseText = node.Value.silkLinks[i].LinkText;
+                        
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Silk/Editor/SilkWindow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Silk.Editor {
+    public class SilkWindow : MonoBehaviour {
+        [MenuItem("Silk/Create New Silk")]
+        
+        private static void CreateNewSilk()
+        {
+            if (!GameObject.Find("Silk")) {
+                GameObject newSilkInstance;
+                newSilkInstance = new GameObject("Silk");
+                newSilkInstance.AddComponent<Parser>();
+                newSilkInstance.AddComponent<Importer>();
+            }
+            else {
+                Debug.LogError("There is already one Silk in the scene");
+            }
+        }
+    }
+}
