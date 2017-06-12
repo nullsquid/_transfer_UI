f8c369afa081e97f4b3488e4e9e4e2077388d01d
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-06-12 08:37:06 -0400

    Made new classes to hold up/down response events and events to repopulate the response list once changed

---
+++ Assets/UI/Scripts/ResponseHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ResponseHandler : MonoBehaviour {
+	public UnityEvent OnResponseListChanged;
+	public UnityEvent OnDownKeyPressed;
+	public UnityEvent OnUpKeyPressed;
+	void OnEnable(){
+
+	}
+
+	void OnDisable(){
+
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			OnResponseListChanged.Invoke ();
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			OnDownKeyPressed.Invoke();
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			OnUpKeyPressed.Invoke ();
+		}
+			
+	}
+
+
+}
---
+++ Assets/UI/Scripts/ResponsePrinter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Silk;
+public class ResponsePrinter : MonoBehaviour {
+    //attach to container of responses
+	List<Response> curResponses = new List<Response>();
+
+    public GameObject responsePrefab;
+    
+	public void ChangeResponses(){
+		ClearResponses ();
+		PopulateResponseUI ();
+	}
+
+	void PopulateResponseUI(){
+		//will need to get this from the current silknode in the traversal structure
+		foreach(KeyValuePair<string, SilkGraph> story in Parser.Instance.mother.MotherGraph) {
+			foreach(KeyValuePair<string, SilkNode> node in story.Value.Story) {
+				if(node.Value.GetNodeName() == "Start") {
+					for(int i = 0; i < node.Value.silkLinks.Count; i++) {
+						GameObject newResponse = Instantiate(responsePrefab);
+						AddResponse (newResponse.GetComponent<Response>());
+						//means this script must be attached to the parent object
+						newResponse.transform.SetParent(this.gameObject.transform);
+						newResponse.transform.localScale = new Vector3(1, 1, 1);
+						newResponse.transform.localPosition = new Vector3(newResponse.transform.position.x, newResponse.transform.position.y, 0);
+						newResponse.GetComponent<Response>().responseText = node.Value.silkLinks[i].LinkText;
+
+					}
+				}
+			}
+		}
+	}
+	void ClearResponses(){
+		curResponses.Clear ();
+	}
+
+	void AddResponse(Response newResponse){
+		curResponses.Add (newResponse);
+	}
+
+
+
+}
