b6781ac860314eed5fd50e733339a60fc9eb7370
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-06-17 10:48:22 -0400

    Added Navigation Handler class

--- Assets/UI/Conversation UI/Scripts/ResponsePrinter.cs
+++ Assets/UI/Conversation UI/Scripts/ResponsePrinter.cs
@@ -29,12 +29,11 @@ public class ResponsePrinter : MonoBehaviour {
         }
     }
 	void PopulateResponseUI(){
-
+		//NAV
+		/*
         normalNav.mode = Navigation.Mode.Vertical;
         customNav.mode = Navigation.Mode.Explicit;
-        
-
-
+		*/
         //will need to get this from the current silknode in the traversal structure
 		//TODO roll this loop series into Silk proper
         foreach (KeyValuePair<string, SilkGraph> story in Parser.Instance.mother.MotherGraph) {
@@ -64,42 +63,43 @@ public class ResponsePrinter : MonoBehaviour {
 						}
                         
 					}
-					foreach (Transform child in transform) {
-						switch (child.GetComponent<Response> ().navPos) {
-
-						case Response.NavPosition.TOP:
-							
-							for (int i = 0; i < curDisplayResponses.Count; i++) {
-								Debug.Log ("Hey");
-								if (i == curDisplayResponses.Count - 1) {
-									customNav.selectOnUp = curDisplayResponses [i].GetComponent<Selectable> ();
-								} else if (i == 1) {
-									customNav.selectOnDown = curDisplayResponses [i].GetComponent<Selectable>();
-								}
-							}
-							child.GetComponent<Button>().navigation = customNav;
-
-
-							//customNav.selectOnDown
-							break;
-						case Response.NavPosition.BOTTOM:
-							for (int i = 0; i < curDisplayResponses.Count; i++) {
-								if (i == 0) {
-									customNav.selectOnDown = curDisplayResponses [i].GetComponent<Selectable> ();
-								} else if (i == curDisplayResponses.Count - 2) {
-									customNav.selectOnUp = curDisplayResponses [i].GetComponent<Selectable> ();
-								}
-							}
-							child.GetComponent<Button> ().navigation = customNav;
-							break;
-						case Response.NavPosition.MIDDLE:
-							child.GetComponent<Button> ().navigation = normalNav;
-							break;
-						}
-					}
+//					foreach (Transform child in transform) {
+//						switch (child.GetComponent<Response> ().navPos) {
+//
+//							case Response.NavPosition.TOP:
+//								
+//								for (int i = 0; i < curDisplayResponses.Count; i++) {
+//									Debug.Log ("Hey");
+//									if (i == curDisplayResponses.Count - 1) {
+//										customNav.selectOnUp = curDisplayResponses [i].GetComponent<Selectable> ();
+//									} else if (i == 1) {
+//										customNav.selectOnDown = curDisplayResponses [i].GetComponent<Selectable>();
+//									}
+//								}
+//								child.GetComponent<Button>().navigation = customNav;
+//
+//
+//								//customNav.selectOnDown
+//								break;
+//							case Response.NavPosition.BOTTOM:
+//								for (int i = 0; i < curDisplayResponses.Count; i++) {
+//									if (i == 0) {
+//										customNav.selectOnDown = curDisplayResponses [i].GetComponent<Selectable> ();
+//									} else if (i == curDisplayResponses.Count - 2) {
+//										customNav.selectOnUp = curDisplayResponses [i].GetComponent<Selectable> ();
+//									}
+//								}
+//								child.GetComponent<Button> ().navigation = customNav;
+//								break;
+//							case Response.NavPosition.MIDDLE:
+//								child.GetComponent<Button> ().navigation = normalNav;
+//								break;
+//						}
+//					}
 				}
 			}
 		}
+		this.gameObject.GetComponent<ResponseNavHandler> ().SetCustomNavigation (curDisplayResponses);
 
 	}
 		
