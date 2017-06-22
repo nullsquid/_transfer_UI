500d123820070c51cdbf1b2bf48cef75c86d2465
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-06-21 23:51:44 -0400

    Added shells for methods for commands

--- Assets/UI/Conversation UI/Scripts/CommandPannel/Controller/MainInputHandler.cs
+++ Assets/UI/Conversation UI/Scripts/CommandPannel/Controller/MainInputHandler.cs
@@ -10,18 +10,18 @@ public class MainInputHandler : MonoBehaviour {
     private string _rawText;
     private string _rootCommand;
     #endregion
-    
+
     #region Events
 
     #endregion
     //public string RootCommand { get; set; }
 
     public List<string> argumentList = new List<string>();
-	public GameObject input;
-	#region Unity Methods
-	private void Start(){
-		input = gameObject;
-	}
+    public GameObject input;
+    #region Unity Methods
+    private void Start() {
+        input = gameObject;
+    }
     private void OnEnable() {
         MainInputController.onReturnPressed += OnInputCapture;
 
@@ -30,40 +30,82 @@ public class MainInputHandler : MonoBehaviour {
     private void OnDisable() {
         MainInputController.onReturnPressed -= OnInputCapture;
     }
-	#endregion
+    #endregion
 
-	IEnumerator WaitAndParseInput(){
-		yield return new WaitForEndOfFrame ();
-		_rawText = input.GetComponent<MainInputController> ().ReturnText;
-		ParseInput (_rawText);
-
-	}
-
-    void OnInputCapture() {
-		StartCoroutine (WaitAndParseInput ());
+    IEnumerator WaitAndParseInput() {
+        yield return new WaitForEndOfFrame();
+        _rawText = input.GetComponent<MainInputController>().ReturnText;
+        ParseInput(_rawText);
 
     }
 
-	#region Input Parsing Methods
-	void ParseInput(string textToParse){
-		string[] _rawCommands = _rawText.Split (' ');
+    void OnInputCapture() {
+        StartCoroutine(WaitAndParseInput());
 
-		_rootCommand = _rawCommands [0];
-		ParseInputArgs (_rawCommands);
+    }
 
-	}
+    #region Input Parsing Methods
+    void ParseInput(string textToParse) {
+        string[] _rawCommands = _rawText.Split(' ');
+
+        _rootCommand = _rawCommands[0];
+        ParseInputArgs(_rawCommands);
+
+    }
 
 
-	void ParseInputArgs(string[] _args){
-		if (_args.Length > 1) {
-			for (int i = 1; i < _args.Length - 1; i++) {
-				if (_args[i] != "" || _args[i] != " ")
-					argumentList.Add(_args[i]);
-			}
-		}
-	}
-	#endregion
+    void ParseInputArgs(string[] _args) {
+        if (_args.Length > 1) {
+            for (int i = 1; i < _args.Length - 1; i++) {
+                if (_args[i] != "" || _args[i] != " ")
+                    argumentList.Add(_args[i]);
+            }
+        }
+    }
+    #endregion
 
+    #region Main Methods
+    void HandleInput(string _root, string[] _args) {
+        if (_root == "CONNECT") {
+            ConnectCommand(_args);
+        }
+        else if (_root == "SLEEP") {
+            SleepCommand(_args);
+        }
+    }
+    #endregion
+
+    #region Command Methods
+    void ConnectCommand(string[] _args) {
+
+    }
+
+    void RunCommand(string[] _args) {
+
+    }
+
+    void ScanCommand(string[] _args) {
+
+    }
+
+    void SleepCommand(string[] _args) {
+        if(_args.Length == 0) {
+            //SLEEP
+        }
+        else {
+            //DON'T SLEEP, PRINT ERROR
+        }
+    }
+    void HelpCommand(string[] _args) {
+        if (_args.Length == 0) {
+            //HELP
+        }
+        else if(_args.Length == 1 && _args[0] == "CONNECT"){
+            //PRINT HELP MENU FOR CONNECT
+        }
+
+    }
+    #endregion
 
 
 
