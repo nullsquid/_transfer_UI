2ad0d5d2156abead039344038133ecb92573c003
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-08-18 11:06:16 -0400

    Added controller for buddy list

---
+++ Assets/Components/EndingManager/Scripts/RouteDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteDatabase : MonoBehaviour {
+	private int timesCompleted;
+	private int timesStarted;
+
+	private Dictionary<string,string> beginnings = new Dictionary<string, string>();
+	private Dictionary<string, string> endings = new Dictionary<string, string> ();
+
+	private string curStoryName;
+	private string curNodeName;
+
+	private string startedKey = "TimesStarted";
+	// Use this for initialization
+	void Start () {
+		timesStarted = PlayerPrefs.GetInt(startedKey);
+		PlayerPrefs.SetInt (startedKey, timesStarted += 1);
+		Debug.LogWarning(PlayerPrefs.GetInt(startedKey));
+	}
+
+	public void AddBeginning(string key, string value){
+		beginnings.Add (key, value);
+	}
+
+	public void CacheCurPosition(){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.LogWarning ("CLEARING PLAYER DATA");
+			PlayerPrefs.DeleteAll ();
+		}
+	}
+}
--- Assets/Components/Terminal/Scripts/StateMachine/States/IdleState.cs
+++ Assets/Components/Terminal/Scripts/StateMachine/States/IdleState.cs
@@ -8,16 +8,21 @@ public class IdleState : ITerminalState {
         //exit previous state
         GameObject.FindObjectOfType<Terminal>().canRunCommands = true;
         GameObject.FindObjectOfType<Transfer.Input.MainInputController>().CanRecordInput = true;
+		GameObject.FindObjectOfType<BuddyListController> ().gameObject.SetActive (true);
         Debug.Log("entered IDLE STATE");
 	}
 
 	public void TerminalExecuteState(){
         //just hang out
+		if (GameObject.FindObjectOfType<BuddyListController> ().gameObject.activeSelf == false) {
+			GameObject.FindObjectOfType<BuddyListController> ().gameObject.SetActive (true);
+		}
         Debug.Log("IDLE STATE");
 	}
 
 	public void TerminalExitState(){
         //ummmmmmm
+		GameObject.FindObjectOfType<BuddyListController>().gameObject.SetActive(false);
         Debug.Log("exited IDLE STATE");
 	}
 
