e5f62816cbd98115104da9d327bc43edd3f7766d
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-08-25 20:43:35 -0400

    Tweaked glitch effect, added squares to interface element

--- Assets/UI/Conversation UI/Scripts/ActionLabel.cs
+++ Assets/UI/Conversation UI/Scripts/ActionLabel.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ActionLabel : MonoBehaviour {
     Text text;
+    Text gText;
     public string actionText;
 	// Use this for initialization
 	void Start () {
         text = gameObject.GetComponentInChildren<Text>();
         text.text = actionText;
+        //gText = GameObject.Find("GlitchText").GetComponent<Text>();
+        //gText.text = actionText;
         //gameObject.GetComponent<Image>().color = Color.white;
 	}
 
