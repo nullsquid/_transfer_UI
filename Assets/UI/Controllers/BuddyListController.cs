71f88d384913a7ae5d728301ca19e0c3578af139
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-08-27 17:21:02 -0400

    List updates with every new tree when returning to idle state

--- Assets/UI/Controllers/BuddyListController.cs
+++ Assets/UI/Controllers/BuddyListController.cs
@@ -1,16 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Transfer.Data;
+using Transfer.System;
 
 public class BuddyListController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject connectableName;
+    public GameObject mainStoryName;
+
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            //PopulateBuddyList();
+        }
+    }
+
+    public void StartPopulate() {
+        StartCoroutine(WaitAndPopulateBuddies());
+    }
+    private void PopulateBuddyList() {
+        ClearBuddyList();
+        int numOfOthers = Random.Range(0, CharacterDatabase.GetCharacterCount());
+        int numOfMainStory = Random.Range(0, numOfOthers);
+        List<string> otherNames = new List<string>();
+        foreach (KeyValuePair<string, Character> character in CharacterDatabase.CharacterDictionary) {
+            if (character.Value.Identifier != CharacterManager.instance.GetPlayerID() && character.Value.Name != DialogueManager.instance.connectID) {
+                //newName.GetComponent<Text>().text = "FU
+                otherNames.Add(character.Value.Name);
+            }
+        }
+        for (int i = 0; i < numOfOthers; i++) {
+            if (i == numOfMainStory) {
+                GameObject newMainName = Instantiate(mainStoryName);
+                newMainName.transform.SetParent(gameObject.transform);
+                newMainName.transform.localScale = (new Vector3(1, 1, 1));
+                newMainName.GetComponent<Text>().text = DialogueManager.instance.connectID;
+            }
+            else {
+                int nameIndex = Random.Range(0, otherNames.Count);
+                GameObject newName = Instantiate(connectableName);
+                newName.transform.SetParent(gameObject.transform);
+                newName.transform.localScale = (new Vector3(1, 1, 1));
+
+                newName.GetComponent<Text>().text = otherNames[nameIndex];
+                otherNames.RemoveAt(nameIndex);
+            }
+        }
+
+    }
+
+    IEnumerator WaitAndPopulateBuddies() {
+        yield return new WaitForEndOfFrame();
+        PopulateBuddyList();
+    }
+
+    void ClearBuddyList() {
+        foreach(Transform child in this.transform) {
+            Destroy(child.gameObject);
+        }
+    }
 }
