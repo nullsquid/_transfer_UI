a017505b0ac73939316b8eec704e6688bbd7305c
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-09-15 19:17:12 -0400

    Added stuff to deal with memory files

---
+++ Assets/Components/MemoryManager/Memories/Memory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Transfer.Data;
+enum MemType{
+	TEXT,
+	VIDEO,
+	AUDIO
+}
+public class Memory  {
+	public string textValue;
+	public string videoName;
+	public string audioName;
+	//
+	string playerCharacterID;
+	MemType mType;
+	public Memory(string memType){
+		switch (memType) {
+		case "text":
+			mType = MemType.TEXT;
+			break;
+		case "video":
+			mType = MemType.VIDEO;
+			break;
+		case "audio":
+			mType = MemType.AUDIO;
+			break;
+		}
+
+		playerCharacterID = CharacterDatabase.GetPlayerID ();
+
+	}
+
+	
+}
