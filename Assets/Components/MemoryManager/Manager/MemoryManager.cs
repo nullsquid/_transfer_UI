aa6c1bffce21a96b72bc8bc5bac5d7b9d11517a0
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-09-15 23:08:21 -0400

    Added all memory data as scriptable objects, added memory handler

--- Assets/Components/MemoryManager/Manager/MemoryManager.cs
+++ Assets/Components/MemoryManager/Manager/MemoryManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class MemoryManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static MemoryManager instance;
+    void Awake() {
+
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public List<MemoryData> AMemories = new List<MemoryData>();
+    public List<MemoryData> BMemories = new List<MemoryData>();
+    public List<MemoryData> CMemories = new List<MemoryData>();
+    public List<MemoryData> DMemories = new List<MemoryData>();
+    public List<MemoryData> EMemories = new List<MemoryData>();
+    public List<MemoryData> FMemories = new List<MemoryData>();
+    public List<MemoryData> GMemories = new List<MemoryData>();
+    public List<MemoryData> HMemories = new List<MemoryData>();
+    public List<MemoryData> IMemories = new List<MemoryData>();
+    public List<MemoryData> ZMemories = new List<MemoryData>();
 }
---
+++ Assets/Components/MemoryManager/MemoryData/MemoryData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryData : ScriptableObject {
+
+    public string ID;
+    public string level;
+    public string type;
+    public string contents;
+}
