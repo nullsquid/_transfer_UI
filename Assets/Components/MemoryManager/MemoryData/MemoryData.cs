e8e130514d34c8106fcbc2ed6b7b84eaea6acd40
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-09-16 13:10:03 -0400

    Added memory UI objects

--- Assets/Components/MemoryManager/MemoryData/MemoryData.cs
+++ Assets/Components/MemoryManager/MemoryData/MemoryData.cs
@@ -7,5 +7,7 @@ public class MemoryData : ScriptableObject {
     public string ID;
     public string level;
     public string type;
+	public string linkedFileName;
+	public string memFileName;
     public string contents;
 }
