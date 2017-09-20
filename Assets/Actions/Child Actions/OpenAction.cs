3945982ef72c1ae1b7b7c746707c247c73ddabc0
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-09-20 19:58:51 -0400

    Finished validating 3x trees

--- Assets/Actions/Child Actions/OpenAction.cs
+++ Assets/Actions/Child Actions/OpenAction.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Silk;
-public class PistolAction : ActionBase {
+public class OpenAction : ActionBase {
 	SilkNode destination;
-	public PistolAction(string name, List<string> args){
+	public OpenAction(string name, List<string> args){
 		ActionName = name;
 		Args = args;
 
 		destination = Silky.Instance.mother.GetNodeByName(DialogueManager.instance.CurStory.StoryName, args[0]);
+
 	}
 
-	public override void ExecuteActionLogic() {
-		Debug.Log (destination.nodeName);
+	public override void ExecuteActionLogic(){
 		DialogueManager.instance.FindNextNodeByName(destination.nodeName);
+
 	}
 }
